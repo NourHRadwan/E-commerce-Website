b898c4d620eecd43c41d63911a226c2404eb796e
NourHRadwan <106489456+NourHRadwan@users.noreply.github.com>
2024-04-03 08:59:47 +0100

    Adding and Display new products

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,8 @@ var productCategoryInput = document.getElementById("ProductCategory");
 var productDescriptionInput = document.getElementById("ProductDescription");
 var productImageInput = document.getElementById("ProductImage");
 
+var productsContainerElement = document.getElementById("productsContainerElement")
+
 var productList = [ ];
 
 function addProduct(){
@@ -15,7 +17,7 @@ function addProduct(){
         productImage : productImageInput.files[0].name,
     }
     productList.push(product);
-    console.log(product);
+    displayProduts();
     restProductInputs();
 }
 
@@ -28,3 +30,31 @@ function restProductInputs(){
      productDescriptionInput.value = '';
      productImageInput.value = null;
  }
+
+
+ function displayProduts()
+ {
+    var containerElement = ` `;
+    for(var i = 0; i < productList.length; i++)
+    {
+        containerElement += `<div class="col"> 
+                                <div class="border shadow-sm p-2">
+                                <div class="img-container mb-5">
+                                    <img class="w-100 h-100 object-fit-contain" src="./Images/${productList[i]["productImage"]}" alt="">
+                                </div>
+                                    <h3 class="fs-5">${productList[i].productName}</h3>
+                                    <p class="fs-6 text-secondary">${productList[i].productDescription}</p>
+                                    <p><span class="fw-semibold">Category</span> ${productList[i].productCategory}</p>
+                                    <div class="d-flex justify-content-between pe-3">
+                                        <p class="fw-semibold">${productList[i].productPrice}</p>
+                                    <div>
+                                        <i class="fa-solid fa-trash-can fs-6 text-danger"></i>
+                                        <i class="fa-solid fa-pen-to-square fs-6 text-success"></i>
+                                    </div>
+                                        </div>
+                                </div>
+                            </div>`
+    }
+    productsContainerElement.innerHTML = containerElement;
+ }
+ displayProduts();
